c38bea6bd13382fd81235a6650fbc3faf2a6d4e5
Braifz <braian94lp@gmail.com>
2022-09-16 11:56:02 -0300

    add component cards

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,13 @@
 import Head from "next/head";
 import Image from "next/image";
 import Layout from "../components/Layout";
+import Cards from "../components/Cards";
 import { createClient } from "next-sanity";
 
 export default function Home({ foods }) {
   return (
     <Layout>
-      <h1 className="text-2xl red-300">hello!</h1>
-      {foods.length > 0 && (
-        <ul>
-          {foods.map((food) => (
-            <div key={food._id}>
-              <li>{food?.name}</li>
-              <Image
-                src={food.imageUrl}
-                height="150px"
-                width="150px"
-                alt="image-food"
-              />
-            </div>
-          ))}
-        </ul>
-      )}
+      <Cards foods={foods} />
     </Layout>
   );
 }
