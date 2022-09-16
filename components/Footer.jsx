4aebbf2609314e593cef1dda8756aaec8bad95d5
Braifz <braian94lp@gmail.com>
2022-09-16 10:33:09 -0300

    create archives from layout component

---
+++ components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer>
+      <h2>soy un footer</h2>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header>
+      <h2>Next Sanity demo</h2>
+    </header>
+  );
+};
+
+export default Header;
